e13d82a40f9a59da2d1e7d34875c6206352f4dc9
Devansh Parapalli <parapallidev@gmail.com>
2022-10-29 19:50:43 +0530

    added the video in place of the poster

--- src/lib/components/AboutUs.svelte
+++ src/lib/components/AboutUs.svelte
@@ -1,10 +1,10 @@
 <script>
-  import AboutUs from "$lib/assets/about_us.png";
+  import AboutUs from "$lib/assets/logo.mp4";
 </script>
 
 <h2 class=" text-center w-full mx-auto text-4xl vimh mb-10">About Us</h2>
 <div class="card xl:card-side xl:bg-base-100 shadow-blue-300 ">
-    <figure><img src="{AboutUs}" class="rounded-t-2xl sm:rounded-2xl xl:mt-0 xl:rounded-none max-w-xs md:max-w-md lg:max-w-md xl:max-w-lg aspect-square max-h-96" alt="Album"/></figure>
+    <figure><video loop muted autoplay src="{AboutUs}" class="rounded-t-2xl sm:rounded-2xl xl:mt-0 xl:rounded-none max-w-xs md:max-w-md lg:max-w-md xl:max-w-lg aspect-square max-h-96" alt="Album"/></figure>
     <div class="card-body text-center bg-base-100 sm:mt-4 sm:rounded-2xl xl:rounded-r-xl  xl:bg-transparent max-w-xs md:max-w-md lg:max-w-md xl:max-w-lg">
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse odit ipsa animi voluptate voluptates error magni cum nemo? Cupiditate atque nostrum id distinctio perferendis dolore libero beatae ab. Quam, error? Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi repudiandae numquam temporibus ipsam eos ratione deserunt dolor accusamus soluta velit, id saepe natus. Adipisci, vitae minus dicta iusto labore eaque.</p>
       <div class="card-actions justify-end hidden">
