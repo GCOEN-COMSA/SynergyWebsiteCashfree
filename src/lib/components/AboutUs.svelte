1f204f56ff7d865c1ed1d2daf05066130466640e
Devansh Parapalli <parapallidev@gmail.com>
2022-10-30 23:16:42 +0530

    added sooom comtent

--- src/lib/components/AboutUs.svelte
+++ src/lib/components/AboutUs.svelte
@@ -6,7 +6,13 @@
 <div class="card xl:card-side xl:bg-base-100 shadow-blue-300 ">
     <figure><video loop muted autoplay src="{AboutUs}" class="rounded-t-2xl sm:rounded-2xl xl:mt-0 xl:rounded-none max-w-xs md:max-w-md lg:max-w-md xl:max-w-lg aspect-video lg:aspect-square max-h-96" alt="Album"/></figure>
     <div class="card-body text-center bg-base-100 sm:mt-4 sm:rounded-2xl xl:rounded-r-xl  xl:bg-transparent max-w-xs md:max-w-md lg:max-w-md xl:max-w-lg">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse odit ipsa animi voluptate voluptates error magni cum nemo? Cupiditate atque nostrum id distinctio perferendis dolore libero beatae ab. Quam, error? Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi repudiandae numquam temporibus ipsam eos ratione deserunt dolor accusamus soluta velit, id saepe natus. Adipisci, vitae minus dicta iusto labore eaque.</p>
+      <p>Tracing back its origins, SYNERGY symbolizes ‘striving for excellence by working together’.
+        Combined efforts of our department has brought our techfest in motion.
+      </p><p>
+        Back with more enthusiasm than ever, Synergy is returning with banger events and amazing prizes.So fasten your belts and get ready for an unforgettable journey.
+      </p><p>
+        See you soon!
+        </p>
       <div class="card-actions justify-end hidden">
         <button class="btn btn-primary">Listen</button>
       </div>
