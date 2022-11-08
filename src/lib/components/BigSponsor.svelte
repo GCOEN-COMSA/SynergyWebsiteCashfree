3a7c8606452e60728d72b8b2fb638f3c206fe354
Devansh Parapalli <parapallidev@gmail.com>
2022-11-08 16:15:26 +0530

    externalized links

--- src/lib/components/BigSponsor.svelte
+++ src/lib/components/BigSponsor.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <div class="big-spons-container flex flex-col items-stretch mx-auto">
-    <a href="https://codons.in/" class="big-spons mx-auto mt-4">
+    <a target="_blank" rel="external noopener noreferrer" href="https://codons.in/" class="big-spons mx-auto mt-4">
         <img src="{codon}" alt="Codon's">
     </a>
-    <a href="https://instagram.com/frostandfeatherngp?igshid=YmMyMTA2M2Y=" class="big-spons inline-flex mt-10 self-stretch bg-white">
+    <a target="_blank" rel="external noopener noreferrer" href="https://instagram.com/frostandfeatherngp?igshid=YmMyMTA2M2Y=" class="big-spons inline-flex mt-10 self-stretch bg-white">
         <img class="mx-auto" src="{fnf}" alt="Frost & Feathers">
     </a>
 </div>
