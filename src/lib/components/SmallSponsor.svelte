e3ed1f1cc798db2daea0aca4fd5728ab3fc933bb
Devansh Parapalli <parapallidev@gmail.com>
2022-11-08 16:17:56 +0530

    link wali bakwas

--- src/lib/components/SmallSponsor.svelte
+++ src/lib/components/SmallSponsor.svelte
@@ -5,6 +5,6 @@
 
 <div class="small-spons-container flex flex-row flex-wrap items-center justify-around mx-auto mt-10 gap-10">
     <a target="_blank" rel="external noopener noreferrer" href="https://instagram.com/niobmesports?igshid=YmMyMTA2M2Y=" class="small-spons"><img class="max-w-xs" src="{niobium}" alt="Robo Studio"></a>
-    <a target="_blank" rel="external noopener noreferrer" href="https://www.robostdio.in" class="small-spons"><img class="max-w-xs lg:max-w-lg" src="{robostudio}" alt="Niobium"></a>
+    <a target="_blank" rel="external noopener noreferrer" href="https://robostudio.in/" class="small-spons"><img class="max-w-xs lg:max-w-lg" src="{robostudio}" alt="Niobium"></a>
 </div>
 
